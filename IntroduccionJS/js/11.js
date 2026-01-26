7020ba0839fcee7743470ba2dc834b4e00eb89f4
Juan Pablo Osorio <juanpabloosorioarias@gmail.com>
2026-01-25 20:34:31 -0500

    todos las carpetas de aprendizaje

---
+++ IntroduccionJS/js/11.js
@@ -0,0 +1,10 @@
+//destruction de arrays
+const tecnologias = ['vue.js', 'javascript', 'node.js' , 'HTML', 'CSS']
+
+const [vuejs, js] = tecnologias //la primera con la primera del arreglo y asi va siguindo 
+
+//const [vuejs,  ,  js] = tecnologias ////// en caso de querer saltar un elemento solo se colocan las comas con un espacio
+
+
+console.log(vuejs)
+console.log(js)
---
+++ IntroduccionJS/js/12.js
@@ -0,0 +1,14 @@
+//iteradores en js
+const tecnologias = ['vue.js', 'javascript', 'node.js' , 'HTML', 'CSS']
+
+//forEach 
+tecnologias.forEach(function(tech, i){
+    //console.log(tech )
+    //console.log(i)
+})
+
+//map
+const arrayMap = tecnologias.map(function(tech, i){
+    return tech 
+})
+console.log(arrayMap)
